1d78c8bf322a412e324edb2aed844445eefff171
Daniel Driskill <daniel.driskill@endurance.com>
2019-04-11 16:43:24 -0600

    Add landing page gif image. Position footer properly on each page. Add margin above auth form buttons.

--- src/components/Landing.js
+++ src/components/Landing.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import philosophy from '../assets/images/philosophy.gif';
 
 const Landing = ({ history }) => (
   <div className="landing">
     <div>
+      <img
+        src={philosophy}
+        alt="Mind Blown"
+        className="landing-image"
+      />
       <h1>A dead simple philosophy.</h1>
       <p>
         Organize your daily efforts and set goals with&nbsp;purpose.<br />
--- src/components/Philosophy.js
+++ src/components/Philosophy.js
@@ -12,39 +12,39 @@ const Philosophy = () => (
     <div className="philo-section">
       <h4>Start with 'Why'</h4>
       <p>
-        A worthwhile goal has a higher chance of being realized if you first define a clear root motivator.
+        A worthwhile goal has a higher chance of being realized if you first define a clear root motivator. Your <i>root motivator</i> is the reason you do all that you do, the higher purpose that guides your actions.
       </p>
       <p>
-        Your <i>motivator</i> is the reason you do all that you do. It's the higher purpose that guides your life.
+        The goals you set should reflect your motivators. Take some time to ponder what truly drives you. Consider whether your current goals are working to fulfill these drivers. Course-correct as needed.
       </p>
       <p>
-        The goals you set on Yokr should reflect your motives. Take some time to ponder what truly drives you.
-        Consider whether your current goals fulfill your root motivations. Make updates as needed.
+        An example of a great root motivator is <i>"To become physically fit and healthy in order to boost my confidence."</i>
       </p>
     </div>
 
     <div className="philo-section">
       <h4>Yokr vs. Todo</h4>
       <p>
-        A todo is a task. It's an item to be checked off. It may or may not be helping you fulfill your ultimate purpose.
+        A <i>todo</i> is a task. It's an item to be checked off. It may or may not be helping you reach a goal.
       </p>
       <p>
-        A <i>yokr</i> is not a task, but a goal for achieving a specific outcome. Thinking in terms of 'outcomes' rather than
-        'tasks' leads you to visualize <i>exactly</i> what successful execution looks like. Your level of motivation increases.
-        The difference between success and failure becomes crystal clear. At any time during the process, you can measure your
-        trajectory and course&#8209;correct if needed. No wonder this style of goal&#8209;setting works so well within teams!
+        A <i>yokr</i> isn't a task &mdash; it's a <u>visualization</u> of a specific outcome.
       </p>
       <p>
-        With Yokr, you'll define specific goals in the form of detailed <i>visualizations</i>, then the question of what tasks will get you
-        there is up to you!
+        Focusing on <i>specific outcomes</i> rather than just 'tasks' provides better context as we make decisions, prioritize, and manage our time. At any time during the process, we can measure our trajectory and course&#8209;correct as needed.
+      </p>
+      <p>
+        <i>Visualizations</i> increase our motivation to see something through to the end and establish exactly what successful execution looks like. Whether you have succeed or failed in the end will be crystal clear.
+      </p>
+      <p>
+        A great example of a <i>yokr</i> is <i>"I walk the dog five miles without stopping and break a good sweat. I feel refreshed and happy I did it."</i> This has much more effect than a bland and uninspiring <i>"Walk the dog."</i>
       </p>
     </div>
 
     <div className="philo-section">
       <h4>Daily Goals</h4>
       <p>
-        Break your efforts into manageable, daily chunks. To focus on doing it 'one day at a time' is not just a saying,
-        but wise counsel given throughout history, because it works!
+        Break your efforts into manageable, daily chunks. To focus on doing it 'one day at a time' is not just a saying, but wise counsel given throughout history, because it actually works!
       </p>
     </div>
   </div>
